e5d247b814cf3ff76c791fb122e21ea831eeea13
Visnicio <vinicius.antonio0712@gmail.com>
2022-09-13 15:23:06 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+
+  import Timer from './lib/Timer.svelte'
+  let timer;
+</script>
+
+<main>
+  <Timer bind:this={timer}/>
+
+  <button on:click={()=>timer.pomodoro(1)}>Start Timer</button>
+
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ src/lib/Timer.svelte
@@ -0,0 +1,25 @@
+<script>
+    let seconds = 0;
+    let interval;
+
+    let isCounting = true
+
+    export function pomodoro(mins){
+        
+        seconds = mins*60 || 0; // set the minutes in seconds format
+        interval = setInterval(()=>{
+            seconds --;
+
+            if(!seconds){
+                clearInterval(interval);
+
+                alert("End of time");
+            }
+        },1000)
+    }
+
+</script>
+
+<div>
+    <h1>{time}</h1>
+</div>
