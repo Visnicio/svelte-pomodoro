a31baccbecf0fbb634674b595fce82036a3e9b0a
visnicio <vinicius.antonio0712@gmail.com>
2022-09-14 22:57:30 -0300

    refactor

--- src/App.svelte
+++ src/App.svelte
@@ -6,43 +6,18 @@
   let timer;
 
   let isCounting;
-  let mins;
+  let timeInSeconds;
+
 </script>
 
 <main>
-  <Timer bind:this={timer} bind:mins bind:isCounting/>
+  <button on:click={()=>{isCounting=true}}>Start/Resume</button>
+  <button on:click={()=>{isCounting=false}}>Stop</button>
 
-  {#if isCounting===false}
-    <button on:click={()=>timer.startPomodoro(mins)}>Start Pomodoro</button>
-  {/if}
-  {#if isCounting===true}
-    <button on:click={()=>timer.pausePomodoro(mins)}>Pause</button>
-  {/if}
-  {#if isCounting===true}
-    <button on:click={()=>timer.pausePomodoro()}>Resume</button>
-  {/if}
-  <button on:click={()=>{mins = 25}}>25 Minutes</button>
-  <button on:click={()=>{mins = 5}}>5 Minutes</button>
-  <button on:click={()=>{mins = 15}}>15 Minutes</button>
+  <Timer bind:this={timer} bind:isCounting bind:timeInSeconds/>
 
-  <h4>binding isCounting {isCounting}</h4>
-  <h4>binding mins {mins}</h4>
+  <button on:click={()=>{timeInSeconds=1500}}>25 Minutes</button>
+  <button on:click={()=>{timeInSeconds=300}}>5 Minutes</button>
+  <button on:click={()=>{timeInSeconds=900}}>15 Minutes</button>
 
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
--- src/lib/Timer.svelte
+++ src/lib/Timer.svelte
@@ -1,78 +1,27 @@
 <script>
-    let totalSeconds;
-    let interval;
-
-    export let mins = 0;
-    let sec = 0;
-
+    export let timeInSeconds = 300;
     export let isCounting = false;
 
-    export function pomodoro(totalMins){
-        
-        totalSeconds = totalMins*60 || 0; // set the minutes in seconds format
-        
-        isCounting = true;
+    let minutes;
+    let seconds;
 
-        // interval = setInterval(()=>{
+    let interval;
 
-        //     if(isCounting){
-        //         totalSeconds --;
-
-        //         mins = Math.floor(totalSeconds/60);
-        //         sec = totalSeconds % 60;
-
-        //         if(!totalSeconds){
-        //             clearInterval(interval);
-
-        //             isCounting = false;
-
-        //             let sound = new Audio("../assets/sounds/clock.mp3");
-        //             sound.play();
-        //         }
-        //     }
-        // },1000)
+    $: {
+        seconds = Math.floor(timeInSeconds % 60);
+        minutes = Math.floor(timeInSeconds/60);
     }
 
-    export function pausePomodoro(){
-        isCounting = false;
-    }
-    export function reset(){
-        clearInterval(interval);
-        
-        isCounting = false;
-    }
 
-    export function startPomodoro(totalMins){
-        totalSeconds = totalMins*60 || 0; // set the minutes in seconds format
-        
-        isCounting = true;
-    }
-
-    function formatMinutes(num) {
-        return num.toString().padStart(2, '0');
-    }
-
-    interval = setInterval(()=>{
-
+    interval = setInterval( ()=>{
         if(isCounting){
-            totalSeconds --;
-    
-            mins = Math.floor(totalSeconds/60);
-            sec = totalSeconds % 60;
-    
-            if(!totalSeconds){
-                clearInterval(interval);
-    
-                isCounting = false;
-    
-                let sound = new Audio("../assets/sounds/clock.mp3");
-                sound.play();
-            }
+            timeInSeconds --;
         }
+
     },1000)
 
 </script>
 
 <div>
-    <h1>{formatMinutes(mins)} : {formatMinutes(sec)}</h1>
+    <h1>{minutes.toString().padStart(2, '0')} : {seconds.toString().padStart(2, '0')}</h1>
 </div>
